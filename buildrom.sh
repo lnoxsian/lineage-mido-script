0e7f75664be54ac90c6d35e04a394b19196d27cf
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2017-08-31 04:48:28 +0530

    Remove unnecessary commands
    Also: Display time after build completion

--- buildrom.sh
+++ buildrom.sh
@@ -10,14 +10,13 @@ source build/envsetup.sh
 # Sync Repo
 echo "2. Syncing Repo | $(date)"
 repo sync -j 16
-breakfast mido
 
 # Build ROM
 echo "3. Building ROM | $(date)"
-croot
 brunch mido
 
 # CD to OUT Dir
+echo "Build finished at $(date)"
 echo "Output Directory: "
 cd $OUT
 ls -l
