7d41d6e3ea74e493ff02871b879e97585136a751
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2017-08-31 02:55:24 +0530

    Fix Typo & Missing Quote

--- initialsetup.sh
+++ initialsetup.sh
@@ -4,7 +4,7 @@
 cd ~
 
 # Install Required Packages
-echo "1. Installing Require Packages | $(date)"
+echo "1. Installing Required Packages | $(date)"
 yes | sudo apt-get update
 yes | sudo apt-get upgrade
 yes | sudo apt-get install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
@@ -17,7 +17,7 @@ mkdir -p ~/bin
 mkdir -p ~/android/lineage
 
 # Fetch Android Platform Tools and Repo and to profile
-echo 3. Installing Platform Tools & Repo Command| $(date)"
+echo "3. Installing Platform Tools & Repo Command| $(date)"
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip
 echo "# add Android SDK platform tools to path" >> ~/.profile
