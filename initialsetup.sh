ac9667dfa76867314e0983be86bfc566f0249d6f
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2017-08-31 02:48:37 +0530

    Create Directory prior to Access

--- initialsetup.sh
+++ initialsetup.sh
@@ -11,8 +11,13 @@ yes | sudo apt-get install bc bison build-essential curl flex g++-multilib gcc-m
 yes | sudo apt-get install openjdk-8-jdk
 yes | sudo apt-get install unzip
 
+# Create Directories
+echo "2. Creating Directories | $(date)"
+mkdir -p ~/bin
+mkdir -p ~/android/lineage
+
 # Fetch Android Platform Tools and Repo and to profile
-echo "2. Installing Platform Tools & Repo Command| $(date)"
+echo 3. Installing Platform Tools & Repo Command| $(date)"
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip
 echo "# add Android SDK platform tools to path" >> ~/.profile
@@ -27,13 +32,8 @@ echo "    PATH=\"$HOME/bin:$PATH\"" >> ~/.profile
 echo "fi" >> ~/.profile
 source ~/.profile
 
-# Create Directories
-echo "3. Creating Directories | $(date)"
-mkdir -p ~/bin
-mkdir -p ~/android/lineage
-cd ~/android/lineage
-
 # Initialize Repo
+cd ~/android/lineage
 echo "4. Initializing Repo | $(date)"
 repo init -u https://github.com/LineageOS/android.git -b cm-14.1
 
