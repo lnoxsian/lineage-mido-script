f08c8ec3434aa0f58db6e9a7797e2cb248ddaff5
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2018-05-06 00:07:04 +0530

    Remove  libesd0-dev
    Package is missing in Ubuntu 18.04

--- initialsetup.sh
+++ initialsetup.sh
@@ -7,7 +7,7 @@ cd ~
 echo "1. Installing Required Packages | $(date)"
 yes | sudo apt-get update
 yes | sudo apt-get upgrade
-yes | sudo apt-get install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+yes | sudo apt-get install bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 yes | sudo apt-get install openjdk-8-jdk
 yes | sudo apt-get install unzip
 
